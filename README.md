c051854ed8d448a969ecc6c075b80d89f4a96548
Parakh <parakhm95@gmail.com>
2019-04-09 23:56:07 -0400

    Best 2D Livemap

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Configuration files for using Velodyne VLP-16 for map generation with google car
 
 You have to use my_robot.launch with roslaunch instead of demo_my_robot.launch
 Also, tf map needs to be sorted out and to do that, VLP16 launch file on the robot has to have static_transform_publisher node argument for velodyne.
+
+
+They are fighting right now with each other.
+
+When map to odom transform in velodyne launch is disabled, it performs better. Performs even better with imu enabled.
+
+Description update
+Remove odometry  and use only IMU and Pointcloud.
--- my_robot.lua
+++ my_robot.lua
@@ -20,7 +20,7 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "imu_link",
-  published_frame = "odom",
+  published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
@@ -46,7 +46,7 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
